16a87d3de70d05b140bcee6ea341e37c1add735e
Sean Lynch <techniq35@gmail.com>
2016-01-28 22:42:02 -0500

    Stable Version 0.11.12

--- dist/js-data-sql.js
+++ dist/js-data-sql.js
@@ -252,7 +252,7 @@ module.exports =
 	      options.with = options.with || [];
 	      var table = getTable(resourceConfig);
 	      var query = options && options.transaction || this.query;
-	      return query.select(table + '.*').from(table).where(resourceConfig.idAttribute, (0, _lang.toString)(id)).then(function (rows) {
+	      return query.select(table + '.*').from(table).where(table + '.' + resourceConfig.idAttribute, (0, _lang.toString)(id)).then(function (rows) {
 	        if (!rows.length) {
 	          return _bluebird2.default.reject(new Error('Not Found!'));
 	        } else {
